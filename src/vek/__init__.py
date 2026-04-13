379161e07cba51360904929079171710b20d3197
AVIDS2 <2141325767@qq.com>
2026-04-13 22:20:14 +0800

    release: v0.2.1

--- src/vek/__init__.py
+++ src/vek/__init__.py
@@ -9,7 +9,7 @@ from vek.hooks import AsyncSession as _AsyncSession
 from vek.hooks import hook, wrap
 from vek.session import Session as _Session
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "init", "store", "log", "branch", "fork", "diff", "replay",
     "show", "cat_file", "status", "tag", "fsck", "gc",
